e57e8def74d26ae6770d2c3164e48848679b45ca
Gary Riger <grigerny@aol.com>
2012-05-15 21:03:10 -0400

    using bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'c2e0047660ef4c46bc87b1a1b3213d50a2f0590268dab0842b6ceba4375ffc078ace7daf87cf042256fc5836b5a5e2d484900ffae9748f52a942ca256258c3a3'
